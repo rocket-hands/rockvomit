e0b487df5e4020d5d0d17ca3079a13cc660c0ad1
Jason Hutchens <jasonhutchens@gmail.com>
2017-01-20 22:04:08 +0800

    shipit

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -8,7 +8,17 @@
 
 <script>
   import 'pixi.js'
+  import p2 from 'p2'
   import { Howl } from 'howler'
+
+  // pixi
+  let game = null
+  let sprite = null
+
+  // p2
+  let world = null
+  let body = null
+
   export default {
     data () {
       return {
@@ -20,32 +30,66 @@
           src: [require('assets/wilhelm.webm'), require('assets/wilhelm.mp3')]
         })
         sound.play()
+      },
+      animate (t) {
+        t = t || 0
+        requestAnimationFrame(this.animate)
+        world.step(1 / 60)
+        sprite.position.x = body.position[0]
+        sprite.position.y = body.position[1]
+        sprite.rotation = body.angle
+        game.renderer.render(game.stage)
       }
     },
     mounted () {
       /* eslint-disable no-undef */
-      let application = new PIXI.Application(900, 600, {
+      game = new PIXI.Application(900, 600, {
         view: document.getElementById('viewport')
       })
+
       let texture = PIXI.Texture.fromImage(require('assets/DDaveOhYeaSoCanYouWriteItInAnEmailAndIllGetBackToYou.png'))
-      let nan = new PIXI.Sprite(texture)
-      nan.anchor.x = 0.5
-      nan.anchor.y = 0.5
-      nan.position.x = 100
-      nan.position.y = 100
-      nan.width = 60
-      nan.height = 60
-      nan.interactive = true
-      nan.on('mousedown', this.scream)
-      nan.on('touchstart', this.scream)
-      application.stage.addChild(nan)
+      sprite = new PIXI.Sprite(texture)
+      sprite.anchor.x = 0.5
+      sprite.anchor.y = 0.5
+      sprite.width = 1
+      sprite.height = 1
+      sprite.interactive = true
+      sprite.on('mousedown', this.scream)
+      sprite.on('touchstart', this.scream)
+
+      let container = new PIXI.DisplayObjectContainer()
+      game.stage.addChild(container)
+
+      container.position.x = game.renderer.width / 2
+      container.position.y = game.renderer.height / 2
+      container.scale.x = 100
+      container.scale.y = -100
+
+      container.addChild(sprite)
+
+      world = new p2.World()
+      let shape = new p2.Box({ width: 1, height: 1 })
+      body = new p2.Body({
+        mass: 1,
+        position: [0, 2],
+        angularVelocity: 1
+      })
+      body.addShape(shape)
+      world.addBody(body)
+
+      let planeShape = new p2.Plane()
+      let planeBody = new p2.Body({ position: [0, -1] })
+      planeBody.addShape(planeShape)
+      world.addBody(planeBody)
+
+      this.animate()
     },
     beforeDestroy () {
     }
   }
 </script>
 
-<style lang="styl">
+<style lang="stylus">
   #viewport
     background-color red
     margin 0
